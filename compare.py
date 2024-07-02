19dbf5a43605f5f3b4eb900effd7aa528884f14f
evgeny-kolonsky <126588934+evgeny-kolonsky@users.noreply.github.com>
2024-07-02 17:59:13 +0300

    pdf parser

--- compare.py
+++ compare.py
@@ -13,7 +13,20 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from hebrew_tokenizer.tokenizer import tokenizer
+import pypdf, codecs
+import datetime
 
+
+def get_pdf_text(filename):
+    reader = pypdf.PdfReader(filename)
+    text = [page.extract_text() for page in reader.pages]
+    return '\n'.join(text)
+
+def get_text(filename):
+    
+    with open(filename, 'r') as f:
+        text = f.read()
+    return text
 
 root_folder = 'C:/Users/Evgeny/Downloads/LabCopyFind/LabCopyFind'
 work_folder = f'{root_folder}/temp'
@@ -37,15 +50,6 @@ def preprocess(text):
 #nltk.download('punkt')
 
 
-
-
-
-def get_text(filename):
-    
-    with open(filename, 'r') as f:
-        text = f.read()
-    return text
-
 # Get a list of student files 
 files = [] 
 texts = []
@@ -55,10 +59,10 @@ for dirpath, dirnames, filenames in os.walk(work_folder):
     for filename in filenames:
         if any(excl in filename for excl in exclusions):
             continue
-        if filename.endswith('.csv'):
+        if filename.endswith('.pdf'):
             file_path = f'{dirpath}/{filename}'
             files.append(file_path)
-            txt = get_text(file_path)
+            txt = get_pdf_text(file_path)
             txt = preprocess(txt)
             texts.append(txt)
             print('.', end='')
@@ -85,6 +89,9 @@ def get_attributes(file_path):
     submission_time = os.path.getmtime(file_path)
     return submission_id, student_name, submission_time, filename            
 #%%
+report = 'id_i\t name_i\t dt_i\t file_i\t\
+            id_j\t name_j\t dt_j\t file_j\t\
+            cos_distance\t days_between \n'
 THRESHOLD = 0.75
 for i in range(0, N):
     print('.', end='')
@@ -95,11 +102,16 @@ for i in range(0, N):
         if id_i == id_j:
             continue
 
-        delta_time = abs(time_i - time_j)
+        delta_time = time_i - time_j
         days = delta_time / 60 /60 /24 
-        if days <7:
-            continue
         
         if similarity[i,j] > THRESHOLD:
-            print()
-            print(f'{name_i}, {name_j}, {similarity[i,j]:.2f}, {days:.0f} {file_i} {file_j}')
+            dt_i = str(datetime.datetime.fromtimestamp(time_i))
+            dt_j = str(datetime.datetime.fromtimestamp(time_j))
+            distance = similarity[i,j]
+            report += f'{id_i}\t{name_i}\t{dt_i}\t{file_i}\t\
+                        {id_j}\t{name_j}\t{dt_j}\t{file_j}\t\
+                        {distance:.2f}\t{days:.0f} \n'
+
+with codecs.open('report.txt', 'w', 'utf-8') as f:
+    f.write(report)                        
