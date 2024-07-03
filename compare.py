7e4ffb56ead40ff2e7fa83f76f20fd8529d16ef0
evgeny-kolonsky <126588934+evgeny-kolonsky@users.noreply.github.com>
2024-07-03 15:30:04 +0300

    n-grams 2 and 3; semester filtering

--- compare.py
+++ compare.py
@@ -7,9 +7,6 @@ Created on Mon Jul  1 09:04:19 2024
 
 import os
 from nltk.corpus import stopwords
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from hebrew_tokenizer.tokenizer import tokenizer
@@ -23,9 +20,11 @@ def get_pdf_text(filename):
     return '\n'.join(text)
 
 def get_text(filename):
-    
-    with open(filename, 'r') as f:
-        text = f.read()
+    if filename.endswith('.pdf'):
+        text = get_pdf_text(filename)
+    else:
+        with open(filename, 'r') as f:
+            text = f.read()
     return text
 
 root_folder = 'C:/Users/Evgeny/Downloads/LabCopyFind/LabCopyFind'
@@ -50,68 +49,113 @@ def preprocess(text):
 #nltk.download('punkt')
 
 
-# Get a list of student files 
-files = [] 
-texts = []
-exclusions = ['Magnetic_Field_Measurement']
     
-for dirpath, dirnames, filenames in os.walk(work_folder):
-    for filename in filenames:
-        if any(excl in filename for excl in exclusions):
-            continue
-        if filename.endswith('.pdf'):
-            file_path = f'{dirpath}/{filename}'
-            files.append(file_path)
-            txt = get_pdf_text(file_path)
-            txt = preprocess(txt)
-            texts.append(txt)
-            print('.', end='')
-print('Processed ', len(files))
-            
+def get_attributes(file_path):
+    
+    dirlevels = file_path.replace(work_folder,'').replace('\\', '/').split('/')
+    
+    moodle_name = [d for d in dirlevels if 'assignsubmission_file' in d][0].split('_')
+        
+    semester_id = dirlevels[1] # YYYY.NN
+    filename = dirlevels[-1]
+    student_name = moodle_name[0]
+    submission_id = moodle_name[1]
+    submission_time = os.path.getmtime(file_path)
+    url = f'"file://{file_path}"'
+    # excel-ready version
+    url_excel = f'"=HYPERLINK( ""{file_path}"", ""{filename}"")"'
+    
+    result = {}
+    result['semester_id'] = semester_id
+    result['submission_id'] = submission_id
+    result['student_name'] = student_name
+    result['timestamp'] = submission_time
+    result['url'] = url
+    result['url_excel'] = url_excel
+    result['file_path'] = file_path
+    result['filename'] = filename
+    return result
+
+def build():
+    attributes = []
+    texts = []
+    for dirpath, dirnames, filenames in os.walk(work_folder):
+        for filename in filenames:
+            if filename.endswith('.pdf'):
+                file_path = f'{dirpath}/{filename}'
+                txt = get_text(file_path)
+                txt = preprocess(txt)
+                texts.append(txt)
+                attr = get_attributes(file_path)
+                
+                attr["size_words"] = len(txt.split()) # tokens count
+                attributes.append( attr )
+                
+                print('.', end='')
+    print('Processed ', len(texts))
+    return attributes, texts
+
+attributes, texts = build()        
+
+
 #%%
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(ngram_range=(2,3))
 print('fitting..')
 tfidf = vectorizer.fit_transform(texts)
 similarity = cosine_similarity(tfidf)
 print('Done')
 N = similarity.shape[0]
 
-#%%
-def get_attributes(file_path):
-    dirlevels = file_path.replace('\\', '/').split('/')
-    try:
-        attributes = [d for d in dirlevels if 'assignsubmission_file' in d][0].split('_')
-    except:
-        print('Error: ', dirlevels)
-    filename = dirlevels[-1]
-    student_name = attributes[0]
-    submission_id = attributes[1]
-    submission_time = os.path.getmtime(file_path)
-    return submission_id, student_name, submission_time, filename            
-#%%
-report = 'id_i\t name_i\t dt_i\t file_i\t\
-            id_j\t name_j\t dt_j\t file_j\t\
-            cos_distance\t days_between \n'
-THRESHOLD = 0.75
-for i in range(0, N):
-    print('.', end='')
-    id_i, name_i, time_i, file_i = get_attributes(files[i])
-    for j in range(0, i):
-        id_j, name_j, time_j, file_j = get_attributes(files[j])
 
-        if id_i == id_j:
+#%%
+report = 'semester \t submission_id \t student_name \t when_submitted \t filename \t url \t size_words \t\
+          semester \t submission_id \t student_name \t when_submitted \t filename \t url \t size_words \t\
+          cos_distance \t days_between \n'
+
+THRESHOLD = 0.75 # similarity treshold
+MIN_DAYS_DISTANCE = 1 # minumum time between submissions
+
+semesters_checked = ['2023.01']
+semesters_referenced = ['2022.02', '2022.03']
+
+for i in range(0, N):
+
+    attr_i = attributes[i]
+    
+    if attr_i["semester_id"] not in semesters_checked:
+        continue
+
+    for j in range(0, N):
+        
+        cos_distance = similarity[i,j]
+        if cos_distance < THRESHOLD:
+            continue;
+
+        attr_j = attributes[j]
+        if attr_j["semester_id"] not in semesters_referenced:
             continue
 
-        delta_time = time_i - time_j
-        days = delta_time / 60 /60 /24 
+        ts1, ts2 = attr_i["timestamp"], attr_j["timestamp"]
+        days_distance = (ts1 - ts2) / 60 /60 /24 
         
-        if similarity[i,j] > THRESHOLD:
-            dt_i = str(datetime.datetime.fromtimestamp(time_i))
-            dt_j = str(datetime.datetime.fromtimestamp(time_j))
-            distance = similarity[i,j]
-            report += f'{id_i}\t{name_i}\t{dt_i}\t{file_i}\t\
-                        {id_j}\t{name_j}\t{dt_j}\t{file_j}\t\
-                        {distance:.2f}\t{days:.0f} \n'
+        if abs(days_distance) < MIN_DAYS_DISTANCE:
+            pass
+            
+        dt1, dt2 = str(datetime.datetime.fromtimestamp(ts1)), \
+                   str(datetime.datetime.fromtimestamp(ts2))
+        
+        sem1, sem2   = attr_i["semester_id"],   attr_j["semester_id"]
+        id1, id2     = attr_i["submission_id"], attr_j["submission_id"]
+        stud1, stud2 = attr_i["student_name"],  attr_j["student_name"]
+        file1, file2 = attr_i["filename"],      attr_j["filename"]
+        size1, size2 = attr_i["size_words"],    attr_j["size_words"]
+        url1, url2   = attr_i["url_excel"],     attr_j["url_excel"]
+        
+        report += f'{sem1}\t{id1}\t{stud1}\t{dt1}\t{file1}\t{url1}\t{size1}\t\
+                    {sem2}\t{id2}\t{stud2}\t{dt2}\t{file2}\t{url2}\t{size2}\t\
+                    {cos_distance:.2f}\t{days_distance:.0f} \n'
 
 with codecs.open('report.txt', 'w', 'utf-8') as f:
     f.write(report)                        
+
+print('Done: see report.txt')
